25a0a208b382735bf41f0f43eee67739c8c07b0b
sanoojan <sanoojan24@gmail.com>
2024-02-28 19:54:39 +0400

    154_updated

--- train_ciai_2.sh
+++ train_ciai_2.sh
@@ -5,4 +5,4 @@ python -u main_swap.py \
 --pretrained_model checkpoints/model.ckpt \
 --base configs/FFHQ/v4_reconstruct_img_train_2_step_multi_false_with_LPIPS_src_conds.yaml \
 --scale_lr False \
---resume /l/users/muhammad.haris/Sanoojan/Outputs/models/Paint-by-Example/Target_CLIP_SRC_ID/PBE/FFHQ/2024-02-16T23-11-43_v4_reconstruct_img_train_2_step_multi_false_with_LPIPS/checkpoints/last.ckpt
+--resume /l/users/muhammad.haris/Sanoojan/Outputs/models/Paint-by-Example/Target_CLIP_SRC_ID/PBE/FFHQ/2024-02-16T23-11-43_v4_reconstruct_img_train_2_step_multi_false_with_LPIPS/checkpoints/epoch=000009.ckpt
